d2f376e51e407b6407ebcf3714b10c08a315ec95
Kenneth Spatta <kennethspatta@me.com>
2017-12-06 20:38:21 -0500

    final commit

--- designs.js
+++ designs.js
@@ -1,18 +1,29 @@
-let color, widthGrid, heightGrid;// Select color input
-const tableGrid = $('#pixel_canvas');
-const sizePicker = $('#sizePicker');
 
+const table = document.getElementById("pixel_canvas");
+const colorPicker = document.getElementById("colorPicker");
+let gridHeight, gridWidth;
 
-
-color = $('#colorPicker');
-heightGrid = $('#input_height');
-widthGrid = $('#input_width');
-
-sizePicker.on('submit', function(event) {
-    event.preventDefault();
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(e) {
+    e.preventDefault();
     makeGrid();
 });
 
 function makeGrid() {
-    
+
+  table.innerHTML = '';
+
+  gridHeight = $("#input_height").val();
+  gridWidth = $("#input_width").val();
+
+  for (let i = 0; i < gridHeight; i++) {
+    let row = table.insertRow(i);
+    for (let j = 0; j < gridWidth; j++) {
+      let cell = row.insertCell(j);
+
+      cell.addEventListener("click", function(event) {
+        cell.style.backgroundColor = colorPicker.value;
+      });
+    }
+  }
 }
