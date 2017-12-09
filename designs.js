9c29d7246b1702a9d302eb07773a72319921bb1d
Kenneth Spatta <kennethspatta@me.com>
2017-12-08 22:56:35 -0500

    added comments to js file

--- designs.js
+++ designs.js
@@ -1,14 +1,16 @@
-
+//grab canvas, height, and width
 const table = document.getElementById("pixel_canvas");
 const colorPicker = document.getElementById("colorPicker");
 let gridHeight, gridWidth;
-
 const sizePicker = document.querySelector("#sizePicker");
+
+//add listener for submit button click
 sizePicker.addEventListener("submit", function(e) {
     e.preventDefault();
     makeGrid();
 });
 
+//called to make the grid when submit is clicked
 function makeGrid() {
 
   table.innerHTML = '';
@@ -16,6 +18,9 @@ function makeGrid() {
   gridHeight = $("#input_height").val();
   gridWidth = $("#input_width").val();
 
+  /*loops through height and height and width to add rows
+  and cells, while also placing event listensers on the
+  cells*/
   for (let i = 0; i < gridHeight; i++) {
     let row = table.insertRow(i);
     for (let j = 0; j < gridWidth; j++) {
